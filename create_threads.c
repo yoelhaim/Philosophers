384e4866e85872860756a68f8b6aab98151454db
yoelhaim <youssefelhaimer8@gmail.com>
2022-05-20 14:48:14 +0100

    create threads

---
+++ create_threads.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_threads.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 14:58:48 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/05/20 14:42:56 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+long long get_time()
+{
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int create_tread(t_data *data, t_philo *philo)
+{
+    int i;
+
+    i = -1;
+    data->beginning_time = get_time();
+    while (++i < data->number_of_philosophers)
+    {
+        // philo[i].check_die_time = get_time();
+        if ((pthread_create(&data->philo[i].id_thread, NULL, routine, (void *)(&philo[i]))))
+            return (printf("%s \n", ERROR_TH), 0);
+    }
+    check_dieth(data, philo);
+    return (1);
+}
