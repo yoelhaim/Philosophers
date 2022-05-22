a8528d980e3df5808b3379144753f5ff7fa94d49
yoelhaim <youssefelhaimer8@gmail.com>
2022-05-22 22:26:02 +0100

    check errors

--- philo.c
+++ philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 12:16:18 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/05/20 09:38:13 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/05/22 22:11:42 by pro              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int ac, char *av[])
 {
     t_data data;
-    t_philo philo;
+    // t_philo philo;
 
     if (ac != 5 && ac != 6)
         return (printf("%s", ERROR_AC), 0);
@@ -23,7 +23,7 @@ int main(int ac, char *av[])
         exit(1);
     if (!(init_philo(av, ac, &data)))
         exit(1);
-    if (!(create_tread(&data, &philo)))
+    if (!(create_tread(&data, (&data)->philo)))
         exit(1);
     // printf("%d\n", data.number_of_philosophers);
 }
