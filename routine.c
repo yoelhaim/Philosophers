f50d8ecce453e5036cd8441de4b030beabc467b7
yoelhaim <youssefelhaimer8@gmail.com>
2022-05-23 18:49:25 +0100

    routine  hard code

--- routine.c
+++ routine.c
@@ -3,15 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 09:39:50 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/05/22 22:27:50 by pro              ###   ########.fr       */
+/*   Updated: 2022/05/23 18:46:56 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int taking_fork(t_data *data, t_philo *philo)
+{
+    pthread_mutex_lock(&(data->forks[philo->left_chopstick]));
+    write_message(data, philo->id, "has taken a fork");
+    pthread_mutex_lock(&(data->forks[philo->right_chopstick]));
+    write_message(data, philo->id, "has taken a fork");
+    start_eat(philo->data, philo);
+    pthread_mutex_unlock(&(data->forks[philo->left_chopstick]));
+    pthread_mutex_unlock(&(data->forks[philo->right_chopstick]));
+    if (data->check_eat)
+        return (1);
+    return (0);
+}
+void start_eat(t_data *data, t_philo *philo)
+{
+    pthread_mutex_lock(&(data->eating));
+    write_message(data, philo->id, "is eating");
+    philo->check_die_time = get_time();
+    pthread_mutex_unlock(&(data->eating));
+    (philo->eat)++;
+    ft_usleep(data->time_to_eat, data);
+}
 void *routine(void *philo)
 {
     t_philo *copy_of_philo;
@@ -19,8 +41,22 @@ void *routine(void *philo)
 
     copy_of_philo = (t_philo *)philo;
     data = copy_of_philo->data;
-    (void)data;
-    // printf("routine %lld \n", data->beginning_time);
+    if (data->number_of_philosophers == 1)
+    {
+        write_message(data, copy_of_philo->id, "has taken a fork");
+        ft_usleep(data->time_to_die, data);
+        return (NULL);
+    }
+    if (copy_of_philo->id % 2)
+        usleep(10000);
+    while (!(data->die))
+    {
+        if (taking_fork(data, copy_of_philo))
+            break;
+        write_message(data, copy_of_philo->id, "is sleeping");
+        write_message(data, data->time_to_sleep, "is sleeping ");
+        write_message(data, copy_of_philo->id, "is  thinking");
+    }
 
     return (NULL);
 }
