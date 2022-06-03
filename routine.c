bf7ba36ce152df5224b90a3ab59a35c54af7c7c7
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-03 15:43:34 +0100

    norm routine

--- routine.c
+++ routine.c
@@ -6,57 +6,58 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 09:39:50 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/06/02 13:45:45 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/06/03 15:32:06 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int taking_fork(t_data *data, t_philo *philo)
+int	taking_fork(t_data *data, t_philo *philo)
 {
-    pthread_mutex_lock(&(data->forks[philo->left_chopstick]));
-    write_message(data, philo->id, "has taken a fork");
-    pthread_mutex_lock(&(data->forks[philo->right_chopstick]));
-    write_message(data, philo->id, "has taken a fork");
-    start_eat(philo->data, philo);
-    pthread_mutex_unlock(&(data->forks[philo->left_chopstick]));
-    pthread_mutex_unlock(&(data->forks[philo->right_chopstick]));
-    if (data->check_eat)
-        return (1);
-    return (0);
+	pthread_mutex_lock(&(data->forks[philo->left_chopstick]));
+	write_message(data, philo->id, "has taken a fork");
+	pthread_mutex_lock(&(data->forks[philo->right_chopstick]));
+	write_message(data, philo->id, "has taken a fork");
+	start_eat(philo->data, philo);
+	pthread_mutex_unlock(&(data->forks[philo->left_chopstick]));
+	pthread_mutex_unlock(&(data->forks[philo->right_chopstick]));
+	if (data->check_eat)
+		return (1);
+	return (0);
 }
-void start_eat(t_data *data, t_philo *philo)
+
+void	start_eat(t_data *data, t_philo *philo)
 {
-    pthread_mutex_lock(&(data->eating));
-    write_message(data, philo->id, "is eating");
-    philo->check_die_time = get_time();
-    pthread_mutex_unlock(&(data->eating));
-    (philo->eat)++;
-    ft_usleep(data->time_to_eat, data);
+	pthread_mutex_lock(&(data->eating));
+	write_message(data, philo->id, "is eating");
+	philo->check_die_time = get_time();
+	pthread_mutex_unlock(&(data->eating));
+	(philo->eat)++;
+	ft_usleep(data->time_to_eat, data);
 }
-void *routine(void *philo)
+
+void	*routine(void *philo)
 {
-    t_philo *copy_of_philo;
-    t_data *data;
+	t_philo	*copy_of_philo;
+	t_data	*data;
 
-    copy_of_philo = (t_philo *)philo;
-    data = copy_of_philo->data;
-    if (data->number_of_philosophers == 1)
-    {
-        write_message(data, copy_of_philo->id, "has taken a fork");
-        ft_usleep(data->time_to_die, data);
-        return (NULL);
-    }
-    if (copy_of_philo->id % 2)
-        usleep(10000);
-    while (!(data->die))
-    {
-        if (taking_fork(data, copy_of_philo))
-            break;
-        write_message(data, copy_of_philo->id, "is sleeping");
-        ft_usleep(data->time_to_sleep, data);
-        write_message(data, copy_of_philo->id, "is  thinking");
-    }
-
-    return (NULL);
+	copy_of_philo = (t_philo *)philo;
+	data = copy_of_philo->data;
+	if (data->number_of_philosophers == 1)
+	{
+		write_message(data, copy_of_philo->id, "has taken a fork");
+		ft_usleep(data->time_to_die, data);
+		return (NULL);
+	}
+	if (copy_of_philo->id % 2)
+		usleep(10000);
+	while (!(data->die))
+	{
+		if (taking_fork(data, copy_of_philo))
+			break ;
+		write_message(data, copy_of_philo->id, "is sleeping");
+		ft_usleep(data->time_to_sleep, data);
+		write_message(data, copy_of_philo->id, "is  thinking");
+	}
+	return (NULL);
 }
