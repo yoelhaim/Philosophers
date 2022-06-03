b81ef18da12f762f60a4c799d4c9b4b15b74f6fc
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-03 15:43:57 +0100

    norm header

--- philo.h
+++ philo.h
@@ -6,59 +6,59 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 12:05:33 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/05/25 17:39:47 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/06/03 15:41:50 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
-#define ERROR_PARSE "error parsing"
-#define ERROR_Z_PHILO "error nember the philo is not valid"
-#define ERROR_AC "error number of argument"
-#define ERROR_TH "create threads has feiled  "
+# define PHILO_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# define ERROR_PARSE "error parsing"
+# define ERROR_Z_PHILO "error nember the philo is not valid"
+# define ERROR_AC "error number of argument"
+# define ERROR_TH "create threads has feiled  "
 
 typedef struct s_philo
 {
-    pthread_t id_thread;
-    int id;
-    int eat;
-    int left_chopstick;
-    int right_chopstick;
-    struct s_data *data;
-    long long check_die_time;
+	pthread_t		id_thread;
+	int				id;
+	int				eat;
+	int				left_chopstick;
+	int				right_chopstick;
+	struct s_data	*data;
+	long long		check_die_time;
 
-} t_philo;
+}	t_philo;
 typedef struct s_data
 {
-    pthread_mutex_t *forks;
-    pthread_mutex_t eating;
-    pthread_mutex_t print;
-    int number_of_philosophers;
-    int time_to_eat;
-    int time_to_die;
-    int time_to_sleep;
-    int die;
-    int check_eat;
-    int time_each_philo_must_eat;
-    long long beginning_time;
-    t_philo *philo;
-} t_data;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	eating;
+	pthread_mutex_t	print;
+	int				number_of_philosophers;
+	int				time_to_eat;
+	int				time_to_die;
+	int				time_to_sleep;
+	int				die;
+	int				check_eat;
+	int				time_each_philo_must_eat;
+	long long		beginning_time;
+	t_philo			*philo;
+}	t_data;
 
-int ft_atoi(char *str);
-int check_parse(char **arg);
-int init_philo(char **av, int ac, t_data *data);
-int create_tread(t_data *data, t_philo *philo);
-long long get_time();
-void *routine(void *philo);
-void check_dieth(t_data *data, t_philo *philo);
-void check_eat(t_data *data, t_philo *philo);
-void write_message(t_data *data, int philoid, char *str);
-void ft_usleep(int time_limit, t_data *data);
-void end(t_data *data, t_philo *philo);
-void start_eat(t_data *data, t_philo *philo);
+int			ft_atoi(char *str);
+int			check_parse(char **arg);
+int			init_philo(char **av, int ac, t_data *data);
+int			create_tread(t_data *data, t_philo *philo);
+long long	get_time(void);
+void		*routine(void *philo);
+void		check_dieth(t_data *data, t_philo *philo);
+void		check_eat(t_data *data, t_philo *philo);
+void		write_message(t_data *data, int philoid, char *str);
+void		ft_usleep(int time_limit, t_data *data);
+void		end(t_data *data, t_philo *philo);
+void		start_eat(t_data *data, t_philo *philo);
 #endif
