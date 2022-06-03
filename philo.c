2ac7060c8fd3d80f26b3a520969279f696905615
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-03 15:43:25 +0100

    norm main

--- philo.c
+++ philo.c
@@ -3,27 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 12:16:18 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/05/22 22:11:42 by pro              ###   ########.fr       */
+/*   Updated: 2022/06/03 15:17:25 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-    t_data data;
-    // t_philo philo;
+	t_data	data;
 
-    if (ac != 5 && ac != 6)
-        return (printf("%s", ERROR_AC), 0);
-    if (!(check_parse(av)))
-        exit(1);
-    if (!(init_philo(av, ac, &data)))
-        exit(1);
-    if (!(create_tread(&data, (&data)->philo)))
-        exit(1);
-    // printf("%d\n", data.number_of_philosophers);
+	if (ac != 5 && ac != 6)
+		return (printf("%s", ERROR_AC), 0);
+	if (!(check_parse(av)))
+		exit(1);
+	if (!(init_philo(av, ac, &data)))
+		exit(1);
+	if (!(create_tread(&data, (&data)->philo)))
+		exit(1);
 }
