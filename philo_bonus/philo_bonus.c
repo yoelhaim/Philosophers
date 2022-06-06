c1467fdf833ef468bdfb05c6ee1d79982f1cb324
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-06 13:44:38 +0100

    main programe philo bonus

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -3,16 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   philo_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:26:49 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/06/05 20:51:12 by pro              ###   ########.fr       */
+/*   Updated: 2022/06/06 11:10:02 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 int main(int ac, char **av)
 {
-
+    t_data_b data;
+    
+    if(ac != 5 && ac != 6)
+        return(printf("error arg \n"), 0);
+    if(!(check_parse(av)))
+        return(0);
+    initailise_data(ac, av, &data);
     return (0);
 }
