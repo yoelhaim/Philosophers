00d7f258f3229cfa5584c2bad935a417d6f985a3
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-09 22:32:39 +0100

    main philo

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:26:49 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/06/06 11:10:02 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/06/09 16:35:31 by pro              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 int main(int ac, char **av)
 {
     t_data_b data;
-    
-    if(ac != 5 && ac != 6)
-        return(printf("error arg \n"), 0);
-    if(!(check_parse(av)))
-        return(0);
+
+    if (ac != 5 && ac != 6)
+        return (printf("error arg \n"), 0);
+    if (!(check_parse(av)))
+        return (0);
     initailise_data(ac, av, &data);
+
+    create_pr(&data, (&data)->philo);
     return (0);
 }
