6bcc2f10b0fc183bcb5e6e12f88cfda053266a55
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-09 23:45:32 +0100

    craete prossess update

--- philo_bonus/create_prossess.c
+++ philo_bonus/create_prossess.c
@@ -6,7 +6,7 @@
 /*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 10:25:40 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/06/09 16:53:36 by pro              ###   ########.fr       */
+/*   Updated: 2022/06/09 23:43:19 by pro              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ int create_pr(t_data_b *data, t_philo_b *philo)
     int i;
     (void)philo;
     i = 0;
-    // data->beginning_time = get_time();
+    data->beginning_time = get_time();
     while (i < data->number_of_philosophers)
     {
         data->child_pid[i] = fork();
-        printf("id the fork is => %d \n", i);
+        if (!data->child_pid[i])
+            create_thread(data, &philo[i]);
+        if (data->child_pid[i] == -1)
+        {
+            kill_process(data);
+            return (0);
+        }
+        // printf("id the fork is => %d \n", data->child_pid[i]);
         i++;
     }
-
+    waitpid(-1, NULL, 0);
+    end(data);
     return (1);
 }
