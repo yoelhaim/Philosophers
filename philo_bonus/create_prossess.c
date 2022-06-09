80f2847a291db45e2af94b759557e3b9dbe02dcf
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-09 22:32:16 +0100

    edit create proccess

--- philo_bonus/create_prossess.c
+++ philo_bonus/create_prossess.c
@@ -3,17 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   create_prossess.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 10:25:40 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/06/06 12:43:47 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/06/09 16:53:36 by pro              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-int create_pr(t_data_b *data)
+int create_pr(t_data_b *data, t_philo_b *philo)
 {
-     
-    return(1);
+    int i;
+    (void)philo;
+    i = 0;
+    // data->beginning_time = get_time();
+    while (i < data->number_of_philosophers)
+    {
+        data->child_pid[i] = fork();
+        printf("id the fork is => %d \n", i);
+        i++;
+    }
+
+    return (1);
 }
